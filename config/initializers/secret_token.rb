e1000587efb478136791133a0a1cbc60b6d45075
Carlos Tames <carlost82@gmail.com>
2012-08-31 11:09:24 -0500

    Creamos una app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tunabook::Application.config.secret_token = 'b6b905c967577f2ba1b386cff9f29e1f262379de2218fc6d284cb75d4ad034d9370b1b579a2182b6b1b761ed7ae29dd2d4cd7240fd6727d1cbe25cf11a958972'
